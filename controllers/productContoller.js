c76c7bf2936bbdd62001b9aee4db929f22d9c17c
hirunimadhushani <hirunimadushani27@gmail.com>
2025-12-11 09:49:10 +0530

    day16

--- controllers/productContoller.js
+++ controllers/productContoller.js
@@ -29,6 +29,7 @@ export function createProduct(req,res){
 }
 
 export function getAllProducts(req,res){
+    console.log("products fetching")
     if(isAdmin(req)){
         Product.find().then(
             (products)=>{
